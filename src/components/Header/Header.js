f62c40992aa7f583def7cf6bd09b43f2dd51b1e6
Henrique Craveiro <hocraveiro@Henriques-MacBook-Pro.local>
2019-07-04 17:58:35 -0300

    fixing logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import styles from './header.module.scss';
-
+import logo from '../../logo.svg';
 function Header (props) {
   return (
     <div className="header">
       <nav className="navbar navbar-dark bg-dark">
-        <a className="navbar-brand" href="/"><img className={styles.logo} alt="app logo" src="/icon.svg"></img> TODO CRUD</a>
+        <a className="navbar-brand" href="/"><img className={styles.logo} alt="app logo" src={logo}></img> TODO CRUD</a>
       </nav>
     </div>
   );
