8bc8555e18858a4bbecc70d144372ff21376e1e2
Henrique Craveiro <hocraveiro@gmail.com>
2019-07-05 00:53:04 -0300

    add method on form test

--- src/components/Form/Form.test.js
+++ src/components/Form/Form.test.js
@@ -10,17 +10,17 @@ const todo = {
 
 describe('<Form.test />', () => {
   it('should render form using as a root body', () => {
-    const wrapper = mount(<Form isOpen={true} handleOnClose={jest.fn} todo={todo}/>);
+    const wrapper = mount(<Form isOpen={true} handleOnClose={jest.fn} todo={todo} handleOnSaveTodo={jest.fn}/>);
   });
 
   it('should render id and description when todo is passed as a prop', () => {
-    const wrapper = mount(<Form isOpen={true} handleOnClose={jest.fn} todo={todo}/>);
+    const wrapper = mount(<Form isOpen={true} handleOnClose={jest.fn} todo={todo} handleOnSaveTodo={jest.fn}/>);
     const text = wrapper.find('h1').text();
-    expect(text).toContain('#123 - To buy beer');
+    expect(text).toContain('Tarefa #123');
   });
 
   it('should render Nova Tarefa when todo is not passed as a prop', () => {
-    const wrapper = mount(<Form isOpen={true} handleOnClose={jest.fn}/>);
+    const wrapper = mount(<Form isOpen={true} handleOnClose={jest.fn} handleOnSaveTodo={jest.fn}/>);
     const text = wrapper.find('h1').text();
     expect(text).toContain('Nova tarefa');
   });
@@ -28,7 +28,7 @@ describe('<Form.test />', () => {
 
   it('should call handleOnClose when clicked on catchclicker', () => {
     const mockOnClose =  jest.fn();
-    const wrapper = mount(<Form isOpen={true} handleOnClose={mockOnClose}/>);
+    const wrapper = mount(<Form isOpen={true} handleOnClose={mockOnClose} handleOnSaveTodo={jest.fn}/>);
     wrapper.find('.ReactModal__Overlay').simulate('click');
     expect(mockOnClose).toBeCalled();
   })
